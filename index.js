9407b86708c91ea8734ed6e1fb9184827ada3fe5
ramanandapanda <ramanandapanda@hotmail.com>
2015-09-16 00:06:45 +0530

    implemented additional field for name

--- index.js
+++ index.js
@@ -5,27 +5,36 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
 });
+app.get('/google', function(req, res){
+   res.sendFile(__dirname + '/google/google.html');
+});
+app.get('/googlelogo_color_272x92dp.png', function(req, res){
+   res.sendFile(__dirname + '/google/googlelogo_color_272x92dp.png');
+});
+app.get('/js/jquery-1.11.1.js', function(req, res){
+   res.sendFile(__dirname + '/lib/jquery-1.11.1.js');
+});app.get('/js/socket.io-1.2.0.js', function(req, res){
+   res.sendFile(__dirname + '/lib/socket.io-1.2.0.js');
+});
 
 io.on('connection', function(socket){
 	var caddress = socket.handshake.address;
-	
-   
+
   socket.on('chat message', function(msg){
     io.emit('chat message',getFormattedTime()+' : '+msg);
   });
-  console.log("New connection from " + caddress );
+  console.log("New connection from " + caddress+'@'+getFormattedTime() );
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
 
-http.listen(3005, function(){
-  console.log('listening on *:3005');
+http.listen(5000, function(){
+  console.log('listening on *:5000');
 });
 
 function getFormattedTime(){
 	var MyDate = new Date();
-	MyDate.setDate(MyDate.getDate() + 20);
 	return ('0' + MyDate.getDate()).slice(-2) + '.' + ('0' + (MyDate.getMonth()+1)).slice(-2) + '.' + MyDate.getFullYear()+' '+MyDate.getHours()+':'+MyDate.getMinutes()+':'+MyDate.getSeconds();
  
 	
